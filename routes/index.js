527f50bd06af6147df86d039c866b5ddb8175853
イ・ドンヨン <lee-dongyeon@kanrishanoMacBook-Air.local>
2020-04-06 12:01:09 +0900

    hello

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,17 @@ router.get('/', async function (req, res, next) {
   if(req.session.login == undefined){
     res.redirect('/users/login');
   }
-  let sql = "select *,datetime(finished,'+9 hours) from todo where user_id=" + req.session.login.id + ' and checked = 0 and finished > CURRENT_TIMESTAMP oreder by finished asc limit 10';
+  let sql = "select *,datetime(finished,'+9 hours') from todo where user_id=" + req.session.login.id + ' and checked = 0 and finished > CURRENT_TIMESTAMP order by finished asc limit 10';
   let records = await dball.getAllRows(sql);
-  let sql2 = "select *,datetime(finished,'+9 hours) from todo where user_id=" + req.session.login.id + ' and checked = 0 and finished < CURRENT_TIMESTAMP order by finished desc limit 10';
+  let sql2 = "select *,datetime(finished,'+9 hours') from todo where user_id=" + req.session.login.id + ' and checked = 0 and finished < CURRENT_TIMESTAMP order by finished desc limit 10';
   let records2 = await dball.getAllRows(sql2);
+  console.log(sql2);
+  console.log(sql);
+  console.log(records);
+  console.log(records2);
+  
 
+  
   res.render('index',{
     title: 'todo',
     login: req.session.login,
